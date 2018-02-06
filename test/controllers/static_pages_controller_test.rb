b7c3b3ee7eb3dd0b21c105f63c67fe5c9c16edb2
Marisa Ramon <mer0522@gmail.com>
2018-02-05 17:44:26 -0800

    Create custom helper for full title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,15 +5,16 @@ require 'test_helper'
 # end
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get root" do
-    get '/'
+    get "/"
     assert_response :success
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
