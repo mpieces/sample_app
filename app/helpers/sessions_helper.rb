f375a51e8c8aafc4ee4dbec6cab94a4c087cfd18
Marisa Ramon <mer0522@gmail.com>
2018-04-04 16:32:22 -0700

    Enable user to edit profile, make new and edit forms into a partial

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -15,6 +19,17 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects back to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the UrL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
