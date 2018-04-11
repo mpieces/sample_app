3149b6755aa8ab80a4a008aa6117a0634a72cbce
Marisa Ramon <mer0522@gmail.com>
2018-04-10 20:47:55 -0700

    Reorganize links and pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to items_path
     else
       render 'new'
     end
